3db3181e5125b335961d8695fca55138e36eb4e9
nawawa <nawaryoga@gmail.com>
2023-07-23 17:09:23 +0900

    fix: スタイル調整
    最初の子要素ならmargin-topを適用しない

--- app/routes/blogs/components/parts/H2.tsx
+++ app/routes/blogs/components/parts/H2.tsx
@@ -4,6 +4,8 @@ type Props = {
 
 export default function H2({ children }: Props) {
   return (
-    <h2 className="text-xl sm:text-2xl font-bold mb-6 sm:mb-8 mt-16 sm:mt-20">{children}</h2>
+    <h2 className="text-xl sm:text-2xl font-bold mb-6 sm:mb-8 mt-16 sm:mt-20 last:mb-0 first:mt-0">
+      {children}
+    </h2>
   );
 }
