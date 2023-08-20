d83d98f9558bc167f2086eb90f6e2e68eefa20d3
nawawa <nawaryoga@gmail.com>
2023-08-20 18:07:49 +0900

    ref

--- app/components/Routes/blogs/components/materials/Content.tsx
+++ app/components/Routes/blogs/components/materials/Content.tsx
@@ -1,4 +1,4 @@
-import parse, { Element, domToReact, attributesToProps } from "html-react-parser";
+import parse, { Element, domToReact } from "html-react-parser";
 import type { HTMLReactParserOptions } from "html-react-parser";
 import type { MovieData } from "~/libs/micro-cms/client.server";
 import Movie from "./Movie";
@@ -20,15 +20,13 @@ export default function Content({ content, movies }: Props) {
   const options: HTMLReactParserOptions = {
     replace: (domNode) => {
       if (domNode instanceof Element && domNode.type === "tag") {
-        const props = attributesToProps(domNode.attribs);
-
-        if (domNode.attribs && domNode.name === "h2") {
+        if (domNode.name === "h2") {
           return <H2>{domToReact(domNode.children, options)}</H2>;
         }
-        if (domNode.attribs && domNode.name === "h3") {
+        if (domNode.name === "h3") {
           return <H3>{domToReact(domNode.children, options)}</H3>;
         }
-        if (domNode.attribs && domNode.name === "p") {
+        if (domNode.name === "p") {
           const containsSpan = domNode.children.some(
             (child) => child.type === "tag" && child.name.toLowerCase() === "span",
           );
@@ -42,7 +40,7 @@ export default function Content({ content, movies }: Props) {
             ) as Element;
             const dataType = spanElement.attribs.class;
             const textNode = spanElement.children[0];
-            if (dataType === "movie-card" && textNode.nodeType === 3 && movies) {
+            if (dataType === "movie-card" && textNode.type === "text" && movies) {
               const movie = movies.filter((movie) => movie.title === textNode.data)[0];
               return <Movie movie={movie} />;
             }
@@ -51,31 +49,7 @@ export default function Content({ content, movies }: Props) {
           const containsAnchor = domNode.children.some(
             (child) => child.type === "tag" && child.name.toLowerCase() === "a",
           );
-
-          if (containsAnchor) {
-            const aElement = domNode.children.find(
-              (child) =>
-                child.type === "tag" &&
-                child.name === "a" &&
-                child.children[0].type === "text" &&
-                child.attribs,
-            ) as Element;
-
-            const child = aElement.children[0];
-
-            // TODO: RichLink
-
-            // if (child.type === "text" && child.data === aElement.attribs.href && urlOgDataMap) {
-            // return (
-            //   <RichLink
-            //     url={aElement.attribs.href}
-            //     ogData={urlOgDataMap.get(aElement.attribs.href)}
-            //   />
-            // );
-            // } else {
-            //   return <P>{domToReact(domNode.children, options)}</P>;
-            // }
-          } else {
+          if (!containsAnchor) {
             return <P>{domToReact(domNode.children, options)}</P>;
           }
         }
