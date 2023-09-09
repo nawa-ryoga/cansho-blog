c6b3ef40ff48709aedfdfeb921e8fa7cf8e1f1e9
nawawa <nawaryoga@gmail.com>
2023-09-09 13:07:13 +0900

    fix

--- app/components/Layouts/Header/parts/Container/index.tsx
+++ app/components/Layouts/Header/parts/Container/index.tsx
@@ -25,12 +25,14 @@ export default function HeaderContainer({
       <div className="w-full px-4">
         <div className="mx-auto max-w-3xl">
           <div className="mx-auto max-w-3xl flex items-center justify-between">
-            <div className="flex justify-between gap-6 header-slide">
+            <div className="flex sm:items-start items-center justify-between gap-6 header-slide">
               {logoMark}
-              <Title
-                display={`block`}
-                pageTitle={pageTitle}
-              />
+              <div className="sm:pt-2">
+                <Title
+                  display={`sm:block ${pageTitle === "CANSHO" ? "block" : "hidden"}`}
+                  pageTitle={pageTitle}
+                />
+              </div>
             </div>
             {internalLink && (
               <div className="flex items-center content-slide">
@@ -41,24 +43,24 @@ export default function HeaderContainer({
             )}
           </div>
           {blogPublished && (
-            <div className="flex items-center gap-6">
-              <div className="h-6 sm:w-12"></div>
+            <div className="flex items-center gap-6 pt-2">
+              <div className="h-2 sm:h-6 sm:w-12"></div>
               <p className={`hidden sm:block tracking-widest header-second-slide text-lg`}>
                 {blogPublished}
               </p>
             </div>
           )}
         </div>
+        {blogPublished && (
+          <div className="flex flex-col gap-2 mb-[0.2rem] sm:mb-[0.4rem]">
+            <Title
+              display={`block sm:hidden`}
+              pageTitle={pageTitle}
+            />
+            <p className={`block sm:hidden header-second-slide`}>{blogPublished}</p>
+          </div>
+        )}
       </div>
-      {blogPublished && (
-        <div className="flex items-center gap-6">
-          <Title
-            display={`block sm:hidden`}
-            pageTitle={pageTitle}
-          />
-          <p className={`block sm:hidden header-second-slide`}>{blogPublished}</p>
-        </div>
-      )}
     </header>
   );
 }
